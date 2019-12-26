36612aaf1ce9114cf5570c46620e7bec9fcccda4
sifu <2587107273@qq.com>
2019-12-26 16:32:51 +0800

    init

---
+++ packages/datePicker/src/datePicker.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>这是一个datePicker组件</div>
+</template>
+
+<script>
+export default {
+  name: "datePicker",
+  props: {},
+  data() {
+    return {};
+  },
+  mounted() {},
+  methods: {},
+  computed: {}
+};
+</script>
+
+<style scoped></style>
