d5de410c465589e793005a9880f24015b197d636
sifu <2587107273@qq.com>
2019-12-27 12:47:25 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,41 @@
 module.exports = {
   base: '/sifu-ui/',
-  title: 'Hello VuePress',
+  title: 'sifu-ui',
   description: 'Just playing around',
   themeConfig: {
+    lastUpdated: '最后更新时间',
+    activeHeaderLinks: true,
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Guide', link: '/guide/' },
-      { text: 'GitHub', link: 'https://github.com/Gitsifu' },
-    ]
+      {text: '首页', link: '/'},
+      {text: '组件', link: '/guide/'},
+      {text: 'GitHub', link: 'https://github.com/Gitsifu/sifu-ui'},
+    ],
+    sidebar: {
+      '/guide/': [
+        {
+          title: '开发指南',   // 必要的
+          collapsable: false, // 可选的, 默认值是 true,
+          sidebarDepth: 1,    // 可选的, 默认值是 1
+          children: [
+            'install', 'quick-start', 'i18n', 'custom-theme'
+          ]
+        },
+        {
+          title: '组件',   // 必要的
+          collapsable: false, // 可选的, 默认值是 true,
+          sidebarDepth: 1,    // 可选的, 默认值是 1
+          children: [
+            'components/button',
+            {
+              title: '基础组件',
+              collapsable: false,
+              children: [
+                'control/layer-list',
+              ]
+            }
+          ]
+        }
+      ]
+    }
   }
 }
